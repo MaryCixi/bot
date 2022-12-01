80ba28e2579bc8c39a42b44abde4691f1051eb94
MaryCixi <119600245+MaryCixi@users.noreply.github.com>
2022-12-01 14:08:18 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+
+
+import datetime 
+import time 
+    
+days = "monday"; "thuesday"; "wenesday"; "thursday"; "friday"; "saturday"; "sunday"
+time = time.time()
+
+
+for day in days :
+    if day == "monday" :
+        if time == 9.05 :
+            print("Pensez à émarger")
+    if day == "thusday" :
+        if time == 9.05 :
+            print("Pensez à émarger")
+    if day =="wenesday" :
+        if time == 9.05 :
+            print("Pensez à émarger")
+    if day =="thurday" :
+        if time == 9.05 :
+            print("Pensez à émarger")
+    if day == "friday" :
+        if time == 9.05 :
+            print("Pensez à émarger")
+    else :
+        pass
+    
+
+for day in days :
+    if day == "monday" :
+        if time == 13.35 :
+            print("Pensez à émarger")
+    if day == "thusday" :
+        if time == 13.35 :
+            print("Pensez à émarger")
+    if day =="wenesday" :
+        if time == 13.35 :
+            print("Pensez à émarger")
+    if day =="thurday" :
+        if time == 13.35 :
+            print("Pensez à émarger")
+    if day == "friday" :
+        if time == 13.35 :
+            print("Pensez à émarger")
+    else :
+        pass
+    
+
+for day in days :
+    if day == "friday":
+        if time == 16.59 :
+            print("C'est le week-end, petit apéro ?")
+    else :
+        pass 
+            
+            
